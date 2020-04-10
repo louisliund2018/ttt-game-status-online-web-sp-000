c020a4a8ed38d63d1fb654f1c5228ae8156c6d02
Louis Liu <louis.liu2013@gmail.com>
2020-04-10 18:08:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,27 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+]
+
+def won?(board)
+  position_taken?(board, index) == "X"
+  position_taken?(board, index) == "O"
+  **some kind of enumerable**
+  check for XXX or OOO
+  if .include?(win_combination)
+  if == one of the WIN_COMBINATIONS && (board == [X,X,X] || board == [O,O,O])
+    win_combination
+  else
+    nil
+  end
+end
